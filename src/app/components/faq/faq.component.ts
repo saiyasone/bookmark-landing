75c7fffab57041c9c3be138cd8cd7272f2515029
saiyasone <saiyasone595@gmail.com>
2023-10-02 14:14:16 +0700

    update css in app.css and faq.css

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  styleUrls: ['./faq.component.css'],
 })
-export class FaqComponent implements OnInit {
+export class FaqComponent {
+  questions: any[] = [
+    {
+      id: '1',
+      title: 'What is Bookmark?',
+      answer:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.',
+      active: false,
+    },
+    {
+      id: '2',
+      title: 'How can I request a new browser?',
+      answer:
+        'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.',
+      active: false,
+    },
+    {
+      id: '3',
+      title: 'Is there a mobile app?',
+      answer:
+        'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.',
+      active: false,
+    },
+    {
+      id: '4',
+      title: 'What about other Chromium browsers?',
+      answer:
+        '  Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.',
+      active: false,
+    },
+  ];
 
-  constructor() { }
+  setQuestion(id: string) {
+    const index = this.questions.findIndex((el) => el.id === id);
 
-  ngOnInit(): void {
+    if (this.questions[index].active) {
+      this.questions[index].active = false;
+    } else {
+      this.questions[index].active = true;
+    }
   }
-
 }
